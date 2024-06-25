40474b625547c915c67415deb191fdd79ba8e76d
Ivan <petrovic.valpovo@gmail.com>
2024-06-26 00:01:31 +0100

    add about app

---
+++ about/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("", views.about_page, name="about_page")
+]
---
+++ about/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import About
+# Create your views here.
+def about_page(request):
+    about = About.objects.all()
+    context = {"about": about}
+
+    return render(request, "about/about.html", context)
