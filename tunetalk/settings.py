4babbb68365d739267bd67b916e876af48d78bad
Ivan <petrovic.valpovo@gmail.com>
2024-06-25 19:47:40 +0100

    add secret key

--- tunetalk/settings.py
+++ tunetalk/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n5v@0x257a3)3f74@xi4zn^ph@pose@idu8&h1ynevu2l=j8=7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
