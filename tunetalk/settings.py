9212c81bf026d4475cf5bb7df3b24d004ed66b95
Ivan <petrovic.valpovo@gmail.com>
2024-06-25 19:31:16 +0100

    set up deployment

--- tunetalk/settings.py
+++ tunetalk/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n5v@0x257a3)3f74@xi4zn^ph@pose@idu8&h1ynevu2l=j8=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
