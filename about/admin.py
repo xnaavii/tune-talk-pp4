9a96d3dbc568daa0c5904530698058f3109b2758
Ivan <petrovic.valpovo@gmail.com>
2024-06-26 00:15:02 +0100

    add django summernote to about page

--- about/admin.py
+++ about/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import About
 # Register your models here.
-admin.site.register(About)
+
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
