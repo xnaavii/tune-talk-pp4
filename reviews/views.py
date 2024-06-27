1b050e284d2bfb7d1e884a6d02a875c8489adc54
Ivan <petrovic.valpovo@gmail.com>
2024-06-27 14:00:56 +0100

    display reviews on the reviews page

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from home.models import Review
 # Create your views here.
 def reviews(request):
 
-    test = "Hello World!"
+    reviews = Review.objects.all()
 
-    context = {"test": test}
+    context = {"reviews": reviews}
     return render(request, 'reviews/reviews.html', context)
