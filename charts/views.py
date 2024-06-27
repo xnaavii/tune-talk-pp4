53867af21f2de8bdfd96e13bef7310580d3b8979
Ivan <petrovic.valpovo@gmail.com>
2024-06-27 17:41:00 +0100

    add top rated albums chart

--- charts/views.py
+++ charts/views.py
@@ -4,8 +4,9 @@ from home.models import Album, Review
 # Create your views here.
 def charts(request):
 
-    tests = "TESTING"
-
-    context = {"tests": tests}
+    # Fetch the top 5 albums based on average rating
+    top_albums = Album.objects.annotate(average_rating=Avg('reviews__rating')).filter(average_rating__lte=5)
+    top_albums = top_albums.order_by('-average_rating')[:10]
+    context = {"top_albums": top_albums}
 
     return render(request, "charts/charts.html", context)
