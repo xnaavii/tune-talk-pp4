3e70cf422af0b045b8a0f873d0cbcb81cb4ac828
Ivan <petrovic.valpovo@gmail.com>
2024-06-27 17:48:22 +0100

    add most reviewed albums in charts

--- charts/views.py
+++ charts/views.py
@@ -6,7 +6,11 @@ def charts(request):
 
     # Fetch the top 5 albums based on average rating
     top_albums = Album.objects.annotate(average_rating=Avg('reviews__rating')).filter(average_rating__lte=5)
-    top_albums = top_albums.order_by('-average_rating')[:10]
-    context = {"top_albums": top_albums}
+    top_albums = top_albums.order_by('-average_rating')[:5]
+    
+    # Fetch the top 5 albums based on the number of reviews
+    most_reviewed_albums = Album.objects.annotate(review_count=Count('reviews')).order_by('-review_count')[:5]
+    
+    context = {"top_albums": top_albums, "most_reviewed_albums": most_reviewed_albums}
 
     return render(request, "charts/charts.html", context)
